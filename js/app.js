03b37d454794bffb5161d0e4091b29e20cc2a18c
SebastianPeterAndersson <sebastianandersson@Sebastians-MacBook-Pro.local>
2016-11-12 16:24:09 +0100

    Addling License file

---
+++ js/app.js
@@ -0,0 +1,352 @@
+// ---------------------------------------------------|
+// ---------------------------------------------------|
+// ------------- | Interactive form | ----------------|
+// ---------------------------------------------------|
+// ---------------------------------------------------|
+
+
+// ---------------------------------------------------|
+// ------------------- | Functions | -----------------|
+// ---------------------------------------------------|
+
+// Focusing the first input on entering page
+function focusFirstInput() {
+    $("input:first").focus();
+    console.log("Focusing on the first input");
+}
+
+function creditCardPlaceholder() {
+    $("#cc-num").attr("placeholder", "xxxxxxxxxxxxxxxx");
+}
+
+
+// If the 'other' option is selected, add input for
+// the user to specify his or her own job role
+function manageJobRole() {
+    // Input to add when 'other' option is selected.
+    var input = $("<input id='other-title' type='text' placeholder='Your Title'></input>");
+    $("#title").change(function() {
+        console.log("changed");
+        //1. If the user chooses the 'other' option:
+        if ($("#title").val() === "other") {
+            //2. textfield is added:
+            $('fieldset:first-of-type').append(input);
+            //3. Adding text to allow the user to choose between entering an occupationxwx§
+            input.val("Doesn't matter! But thanks for asking.").select();
+            //4. And the text-field is focused on immediately:
+            $("#other-title").focus();
+        }
+        if ($("#title").val() !== 'other') {
+            input.remove();
+        }
+    });
+}
+
+// Function managing the t-shirt selection:
+function manageTshirt() {
+    //Initially hiding the color div:
+    $("#colors-js-puns").toggle();
+
+    // The Arrays for the two different styles:
+    var jsPuns = jQuery.makeArray($("#color").children().slice(0, 3));
+    var iLove = jQuery.makeArray($("#color").children().slice(3, 6));
+    // Assign the first option (the 'select theme' option) to a variable and
+    // 1. Remove it once It's no longer needed.
+    var firstOption = $("#design option:first");
+    // The design select clicked:
+    $("#design").change(function() {
+        // showing the colors:
+        $("#colors-js-puns").show();
+        // 2:
+        firstOption.remove();
+        // Remove any lingering children:
+        $("#color").children().remove();
+        // JS puns option selected:
+        if ($("#design").val() === 'js puns') {
+            // Appending the proper list:
+            for (var i = 0; i < jsPuns.length; i++) {
+                $("#color").append(jsPuns[i]);
+            }
+            // Heart js option selected:
+        } else if ($("#design").val() === 'heart js') {
+            // Appending the proper list:
+            for (var i = 0; i < iLove.length; i++) {
+                $("#color").append(iLove[i]);
+            }
+        }
+    });
+}
+
+
+function preventDoubleBook() {
+    // Create an array with the checkboxes to make the traversal easier:
+    var activities = $(".activities").children().children().filter("input");
+    var activitiesArr = $.makeArray(activities);
+
+    // Add classes to make it easier to manipulate the checkboxes:
+    $(activitiesArr).eq(1).addClass("tue-9-12");
+    $(activitiesArr).eq(3).addClass("tue-9-12");
+    $(activitiesArr).eq(2).addClass("tue-1-4");
+    $(activitiesArr).eq(4).addClass("tue-1-4");
+
+
+    // When Tuesday 9-12 days interferes with one another, add classes and disable checkbox;
+    $(".tue-9-12").click(function() {
+        // If the checkbox has the class of dis-checked:
+        if (!$(this).hasClass("dis-checked")) {
+            // Disable the other checkbox by disabling checkbox and adding gray color to make it look more disabled:
+            $(".tue-9-12").not(this).attr("disabled", true);
+            $(".tue-9-12").not(this).parent().toggleClass("checkColor");
+            // Add class to let the program know it has been clicked before and therefore should not be changed next time:
+            $(this).toggleClass("dis-checked");
+
+        } else {
+            $(".tue-9-12").not(this).removeAttr("disabled");
+            $(this).toggleClass("dis-checked");
+            $(this).toggleClass("checkColor");
+            $(".tue-9-12").not(this).parent().toggleClass("checkColor");
+        }
+    });
+
+    // When Tuesday 1-4 interferes with one another, add classes and disable checkbox
+    $(".tue-1-4").click(function() {
+        // If the checkbox does not have the class of dis-checked:
+        if (!$(this).hasClass("dis-checked")) {
+            $(".tue-1-4").not(this).attr("disabled", true);
+            $(".tue-1-4").not(this).parent().toggleClass("checkColor");
+            // Add class to let the program know it has been clicked before and therefore should not be changed next time:
+            $(this).toggleClass("dis-checked");
+            // If the checkbox is active and has been clicked before:
+        } else {
+            // Make the other option available again:
+            $(".tue-1-4").not(this).removeAttr("disabled");
+            $(this).toggleClass("dis-checked");
+            $(this).toggleClass("checkColor");
+            $(".tue-1-4").not(this).parent().toggleClass("checkColor");
+        }
+    });
+}
+
+// Function to dynamically add the current cost of the conference:
+function incrementPrice() {
+    // Initially start at zero:
+    var priceCounter = 0;
+    //1. Constructing initial price counter:
+    var currentPrice = $("<div>" + "$" + priceCounter + "</div>");
+    // Appending it:
+    $(".activities").append(currentPrice);
+
+    $(".activities input").click(function() {
+        // Get the price from the label HTML by slicing the part where the price is shown:
+        var thisPrice = $(this).parent().text().slice(-3);
+        var thisPriceInt = parseInt(thisPrice);
+        //If the checkbox has previously been clicked, we don't want the
+        // price to go up, we want it to go down:
+        if ($(this).hasClass("checked")) {
+            priceCounter -= thisPriceInt;
+        } else {
+            priceCounter += thisPriceInt;
+        }
+        // Update the current price depending on what was clicked:
+        currentPrice.text("$" + priceCounter);
+        $(this).toggleClass("checked");
+    });
+}
+
+function managePayment() {
+
+    $("#payment").val("credit card");
+    var paymentContainer = $("fieldset:last-of-type");
+
+    function showEverything() {
+        $(paymentContainer).find("p").eq(1).show();
+        $(paymentContainer).find("p").eq(0).show();
+        $("#credit-card").show();
+    }
+
+    // function to hide bitcoin
+    function hideBitcoin() {
+        $(paymentContainer).find("p").eq(1).hide();
+    }
+
+    // function to hide paypal
+    function hidePayPal() {
+        $(paymentContainer).find("p").eq(0).hide();
+    }
+
+    function hideCreditCard() {
+        $("#credit-card").hide();
+    }
+
+    hidePayPal();
+    hideBitcoin();
+
+    $("#payment option[value='select_method']").remove();
+
+
+    $("#payment").change(function() {
+
+        showEverything();
+        if ($("#payment").val() === 'paypal') {
+            hideBitcoin();
+            hideCreditCard();
+        } else if ($("#payment").val() === 'bitcoin') {
+            hideCreditCard();
+            hidePayPal();
+        } else if ($("#payment").val() === 'credit card') {
+            hidePayPal();
+            hideBitcoin();
+        }
+    });
+
+}
+
+// ---------------------------------------------------|
+// ------------------- | VALIDATION | ----------------|
+// ---------------------------------------------------|
+
+// ---------------------------------------------------|
+// -------------- | Callback Functions | -------------|
+// ---------------------------------------------------|
+
+// This is a callback function for the 'validateForm'-function.
+function validateNameFields() {
+    if ($("#name").val() === "") {
+        console.log("One of the namefields are not filled in.");
+        window.scrollTo(0, 0);
+        $("#name").attr("placeholder", "Please write your name.").focus();
+        $("#name").blur("on", function() {
+            $(this).attr("placeholder", "");
+        });
+        return false;
+
+    } else {
+        return true;
+    }
+}
+
+// This is a callback function for the 'validateForm'-function.
+function validateEmail() {
+    var email = $("#mail").val();
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email) === false) {
+
+        console.log("Insert a correct email");
+        window.scrollTo(0, 0);
+        $("#mail").attr("placeholder", "Please insert a correct email.").focus();
+        $("#mail").blur("on", function() {
+            $(this).attr("placeholder", "");
+        });
+    }
+    return re.test(email);
+}
+
+// This is a callback function for the 'validateForm'-function.
+function validateActivities() {
+    // Variable to determine how many checkboxes that have been checked:
+    var checkboxesChecked = 0;
+    $("input[type='checkbox']").each(function() {
+        if ($(this).hasClass("checked")) {
+            checkboxesChecked++;
+        } else {}
+    });
+    // If no checkbox is checked:
+    if (checkboxesChecked === 0) {
+        // Jumps to the job role because of the offset on the 'job-role'-id.
+        location.href = "#job-role";
+        $("#locate-activities").text("Please choose atleast one activity.").css("color", "red");
+        console.log("No checkboxes!");
+        return false; //Something is wrong
+        // FALSE CALL:
+
+    } else {
+        $("#locate-activities").text("Register for Activities").css("color", "#184f68");
+        return true; //All good
+    }
+}
+
+// This is a callback function for the 'validateForm'-function.
+function validatePayment() {
+    // If the user has not choosen the credit card option,
+    // there is no use for the function to move on.
+    if ($("#payment").val() !== "credit card") {
+        return true;
+    }
+
+    if ($("#zip").val() === "" || $("#cvv").val() === "") {
+        console.log("One of the payment fields are not filled in");
+        $("#zip").attr("placeholder", "Enter zip").focus().val("");
+        $("#zip").blur("on", function() {
+            $(this).attr("placeholder", "");
+        });
+        return false;
+    } else {
+        return true;
+    }
+}
+// This is a callback function for the 'validateForm'-function.
+function validateCreditCard() {
+    // If the payment option is not credit card,
+    // there is no need for the function to continue.
+    if ($("#payment").val() !== "credit card") {
+        return true;
+    }
+    // Credit card number:
+    var creditCard = $("#cc-num").val();
+    var creditCardInt = parseInt(creditCard);
+    // I must say that I am not that familiar with regular expressions.
+    // This one i snipped of the internet:
+    var re = /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/;
+    if (re.test(creditCardInt) === false) {
+        $("#cc-num").attr("placeholder", "No dashes, no spaces.").focus().val("");
+        $("#cc-num").blur("on", function() {
+            $(this).attr("placeholder", "");
+        });
+        console.log("Credit card not syntactically correct");
+    }
+    return re.test(creditCardInt);
+}
+
+
+// The function that checks with every callback ------2------
+function validateForm() {
+    $("button").click(function(event) {
+        if (validateNameFields() === true && validateEmail() === true && validateActivities() === true && validateCreditCard() === true && validatePayment() === true) {
+            console.log("Validation Passed");
+        } else {
+            event.preventDefault();
+            console.log("You shall not pass!");
+        }
+    });
+}
+
+
+// // The function that checks with every callback ------2------
+// function validateForm() {
+//     $("button").click(function(event) {
+//         if (validateNameFields() === true && validateEmail() === true && validateActivities() === true && validateCreditCard() === true) {
+//             console.log("Validation Passed");
+//         } else {
+//             event.preventDefault();
+//             console.log("You shall not pass!");
+//         }
+//     });
+// }
+
+
+
+
+// ---------------------------------------------------|
+// ------------ | When page is rendered: | -----------|
+// ---------------------------------------------------|
+
+$(document).ready(function() {
+    focusFirstInput();
+    manageJobRole();
+    manageTshirt();
+    preventDoubleBook();
+    incrementPrice();
+    managePayment();
+    validateForm();
+    creditCardPlaceholder();
+});
